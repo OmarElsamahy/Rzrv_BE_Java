339c9b78ce28699fa1b7fc25b67052d37caf5723
ElSamahy, Omar <oelsamahy@deloitte.com>
2024-06-14 15:59:02 +0300

    first commit

---
+++ src/main/java/com/sm7/rzrv/RzrvApplication.java
@@ -0,0 +1,13 @@
+package com.sm7.rzrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RzrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm7/rzrv/RzrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm7.rzrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
